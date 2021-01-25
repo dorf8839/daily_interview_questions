11d5f2f598cc1665c526549c21c809e1598b8fc4
dorf8839 <cory.dorfner@gmail.com>
2021-01-25 08:49:08 -0600

    Formatted code base to include the original question AND the question using an int array.

--- UniqueWaysToClimbStairs/src/com/company/Main.java
+++ UniqueWaysToClimbStairs/src/com/company/Main.java
@@ -5,8 +5,15 @@ public class Main {
     public static void main(String[] args) {
         int totalSteps = 5;
         int[] stepCount = new int[]{1,3,5};
-        int results = staircase(totalSteps, stepCount);
-        System.out.println("Total combinations = " + results);
+        System.out.println("Number of ways to climb the given staircase 1 or 2 steps at a time: " + staircase(totalSteps));
+        System.out.println("Number of ways to climb the given staircase using the given step count: " + staircase(totalSteps, stepCount));
+    }
+
+    private static int staircase(int totalSteps){
+        if(totalSteps <= 1){
+            return 1;
+        }
+        return staircase(totalSteps - 1) + staircase(totalSteps - 2);
     }
 
     private static int staircase(int totalSteps, int[] stepCount){
