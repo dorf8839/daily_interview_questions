a6efe5c752a997a58d546e31d3a928fd7a41545a
Cory Dorfner <cory.dorfner@gmail.com>
2021-03-12 08:12:23 -0600

    Updated to include binary search.

--- Arrays_ReturnElementIndex/src/com/company/Main.java
+++ Arrays_ReturnElementIndex/src/com/company/Main.java
@@ -1,21 +1,36 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> input = new ArrayList<>();
-        input.add(13);
-        input.add(18);
-        input.add(25);
-        input.add(2);
-        input.add(8);
-        input.add(10);
-//        int k = 8;
-        int k = 7;
-        int index = input.indexOf(k);
-        System.out.println("The index of element " + k + " is " + (index>-1 ? index : "NULL"));
+        //Binary Search
+//        int[] my_array = {1,2,3,4,5,6,7};
+        int[] my_array = {7,2,5,3,4,1,6};
+        //Find the index of 5
+        System.out.println("Index position of 5 is: " + findIndex(my_array, 5));
+        //Find the index of 7
+        System.out.println("Index position of 7 is: " + findIndex(my_array, 7));
+        //Linear Search
+//        List<Integer> input = new ArrayList<>();
+//        input.add(13);
+//        input.add(18);
+//        input.add(25);
+//        input.add(2);
+//        input.add(8);
+//        input.add(10);
+////        int k = 8;
+//        int k = 7;
+//        int index = input.indexOf(k);
+//        System.out.println("The index of element " + k + " is " + (index>-1 ? index : "NULL"));
+    }
+
+    public static int findIndex(int[] arr, int t){
+        Arrays.sort(arr);
+        int index = Arrays.binarySearch(arr, t);
+        return (index < 0) ? -1 : index;
     }
 }
