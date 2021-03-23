51cf4e64c903a503e289820f72a67a78fa1e893a
Cory Dorfner <cory.dorfner@gmail.com>
2021-03-23 10:57:42 -0500

    3/22/2021-Adding solution to question asked by Microsoft: Given a positive integer, return the n-th perfect number so that the digits sum up exactly to 10.

---
+++ PerfectNumberGenerator/src/com/corydorfner/Main.java
@@ -0,0 +1,23 @@
+package com.corydorfner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int n = 21032;
+        System.out.println(perfectNumberGenerator(n));
+    }
+
+    public static int perfectNumberGenerator(int n){
+        int sum = 0;
+        int check = 10;
+        String input = String.valueOf(n);
+        for(int i = 0; i < input.length(); i++){
+            sum += Integer.parseInt(String.valueOf(input.charAt(i)));
+        }
+        if(sum < 10) {
+            int remainder = check - sum;
+            return (n * check) + remainder;
+        }
+        return -1;
+    }
+}
